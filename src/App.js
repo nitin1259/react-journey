3921aa620774b92970dc65a258d4772227e238b7
Nitin SINGH <nitinkumar.singh@f5.com>
2021-04-23 20:17:05 +0530

    how react js  react to the jsx element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,12 @@ function App() {
       <h1>I m a React App !!!</h1>
     </div>
   );
+  // compiler will convert over code to below code (behind the scenes)
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "I m a React App !!!")
+  // );
 }
 
 export default App;
