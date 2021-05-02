82f477828d52a1236cc37c1d746c65e18ccb3706
Nitin SINGH <nitinkumar.singh@f5.com>
2021-05-02 14:48:59 +0530

    advance redux - best practice wtih redux

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
-import * as actionTypes from "../actions/actions";
+import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   counter: 0,
@@ -16,15 +17,9 @@ const reducer = (state = initialState, action) => {
         counter: state.counter - 1,
       };
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.val,
-      };
+      return updateObject(state, { counter: state.counter + action.val });
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.val,
-      };
+      return updateObject(state, { counter: state.counter - action.val });
   }
   return state;
 };
