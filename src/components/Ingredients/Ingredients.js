8522cbeb52ada343d0270669ed8a3b2ad1f3305d
Nitin SINGH <nitinkumar.singh@f5.com>
2021-05-03 18:08:58 +0530

    react hooks - useEffect for loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,11 +8,42 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredientList, setIngredientList] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://burger-react-app-3abe8-default-rtdb.firebaseio.com/ing.json"
+      )
+      .then((resp) => {
+        const loadedIngredients = [];
+        // console.log(resp);
+
+        for (const key in resp.data) {
+          loadedIngredients.push({
+            id: key,
+            title: resp.data[key].title,
+            amount: resp.data[key].amount,
+          });
+        }
+        setIngredientList(loadedIngredients);
+      });
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
-    setIngredientList((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    axios
+      .post(
+        "https://burger-react-app-3abe8-default-rtdb.firebaseio.com/ing.json",
+        ingredient
+      )
+      .then((resp) => {
+        // console.log(resp.data.name);
+        setIngredientList((prevIngredients) => [
+          ...prevIngredients,
+          { id: resp.data.name, ...ingredient },
+        ]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const deleteIngredientHandler = (ing_id) => {
